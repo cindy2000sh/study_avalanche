1dffd37f04f7a3970c719b9e18c6e7e8894154fa
cindy2000sh <m13916275080@gmail.com>
2022-01-23 01:34:13 -0500

    update benchmark-V1.py

--- benchmark-V1.py
+++ benchmark-V1.py
@@ -1,12 +1,10 @@
-# benchmark-V1.py
-
 import torch
 import torchvision.transforms as transforms
 
 from avalanche.benchmarks.generators import filelist_benchmark, tensors_benchmark 
 
 
-def get_transforms(is_pretrained):
+def get_transforms(pretrain_feature):
     # copied from https://github.com/ElvishElvis/Continual-Learning/blob/master/load_dataset.py
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                         std=[0.229, 0.224, 0.225])
@@ -24,56 +22,44 @@ def get_transforms(is_pretrained):
             transforms.ToTensor(),
             normalize,
         ])
-        if is_pretrained:
+        if pretrain_feature != "None":
             train_transform, eval_transform = None, None
         return train_transform, eval_transform
 
-# TODO: change load_dataset.py here: https://github.com/ElvishElvis/Continual-Learning/blob/master/load_dataset.py
-# TODO: input == args in original code
-def get_data_set_offline(root, train_list, eval_list, train_transform, eval_transform, pretrained):
-    if pretrained == True:
+def get_data_set_offline(root, train_list, eval_list, train_transform, eval_transform, pretrain_feature):
+    if pretrain_feature == "None":
             generic_scenario = filelist_benchmark(
-            root,  # /data3/zhiqiul/clear_datasets/CLEAR10-TEST-CLEANED/labeled_images
-            train_list, # ["/1/train.txt", ....]
-            eval_list, # ["/1/test.txt",...]
+            root,  
+            train_list, 
+            eval_list, 
             task_labels=range(1,len(train_list)+1), # starts from index 1 because the 0th bucket is used for pretrain
             train_transform=train_transform,
             eval_transform=eval_transform
             )
     else:
-        # pretrained == True
-        # root : "/data3/siqiz/clear_datasets/CLEAR-10-PUBLIC/training_folder/testset_ratio_0.3/seed_0/features/moco_b0"
-        # train_list: ["/1/train.pth", ...]
-        train_feat_lst = list(map(lambda x: root + x, train_list))
-        eval_feat_lst = list(map(lambda x: root + x, eval_list))
         generic_scenario = tensors_benchmark(
-            train_tensors = [torch.load(file) for file in train_feat_lst], 
-            test_tensors = [torch.load(file) for file in eval_feat_lst],
+            train_tensors = [torch.load(file) for file in train_list], 
+            test_tensors = [torch.load(file) for file in eval_list],
             task_labels = range(1,len(train_list)+1)
         )
     return generic_scenario
 
-def get_data_set_online(root, train_list, train_transform, eval_transform, pretrained):
-    if pretrained == False:
+def get_data_set_online(root, train_list, train_transform, eval_transform, pretrain_feature):
+    if pretrain_feature == "None":
         generic_scenario = filelist_benchmark(
             root,  
             train_list,
             train_list,
-            # default evaluation is to evaluate the whole test stream but 
-            # we can only keep the future bucket result during post processing
             task_labels=range(1,len(train_list)+1), # starts from index 1 because the 0th bucket is used for pretrain
             train_transform=train_transform,
             eval_transform=eval_transform
             )
     else:
-        train_feat_lst = list(map(lambda x: root + x, train_list))
         generic_scenario = tensors_benchmark(
-                train_tensors = [torch.load(file) for file in train_feat_lst], 
-                test_tensors = [torch.load(file) for file in train_feat_lst],
+                train_tensors = [torch.load(file) for file in train_list], 
+                test_tensors = [torch.load(file) for file in train_list],
                 task_labels = range(1,len(train_list)+1)
             )
-    # TODO: need to save current model and call eval again for loading private test set [whole test set] on jiashi's train.py
-    # TODO: ask what to compare, since plotting is unavailable
-    # jia's output path: /data/jiashi
+
     return generic_scenario
 
